8a4090b4e6d96ccfc64c4d6dfa62c157967d4028
Nick Paulson <nick@nickpaulson.com>
2010-12-12 09:05:24 -0500

    Now shows a sheet to confirm retweet

--- ReTweetiePlugin.m
+++ ReTweetiePlugin.m
@@ -21,19 +21,9 @@
 
 static void NPCellRepost(id self, SEL _cmd, TweetieStatusTableViewCell *cell, TwitterStatus *status)
 {
-	NSArray *potentialSubviews = [[[cell window] contentView] subviews];
-	NSPredicate *predicate = [NSPredicate predicateWithFormat:@"class == %@", NSClassFromString(@"TweetieAccountTabsView")];
-    NSArray *result = [potentialSubviews filteredArrayUsingPredicate:predicate];
-	if ([result count] > 0) {
-		TweetieAccountTabsView *tabView = [result objectAtIndex:0];
-		TwitterAccount *account = [tabView selectedAccount];
-		if (account != nil) {
-			ABCallback *callback = [NSClassFromString(@"ABCallback") callbackWithTarget:[ReTweetiePlugin sharedInstance] selector:@selector(apiCallback:) info:nil];
-			TwitterAPI *api = [[NSClassFromString(@"TwitterAPI") alloc] initWithAccount:account callback:callback];
-			[api POST:[NSString stringWithFormat:@"statuses/retweet/%@.xml", status.statusID] body:nil callback:callback];
-			
-		}
-	}
+	NSDictionary *info = [[NSDictionary dictionaryWithObjectsAndKeys:cell, @"cell", status, @"status", nil] retain];
+	NSAlert *alert = [NSAlert alertWithMessageText:[NSString stringWithFormat:@"ReTweet"] defaultButton:@"ReTweet" alternateButton:@"Cancel" otherButton:nil informativeTextWithFormat:[NSString stringWithFormat:@"@%@: \"%@\"", [status.fromUser username], status.text]];
+	[alert beginSheetModalForWindow:[cell window] modalDelegate:[ReTweetiePlugin sharedInstance] didEndSelector:@selector(retweetSheetDidEnd:returnCode:contextInfo:) contextInfo:info];
 }
 
 @implementation ReTweetiePlugin
@@ -42,12 +32,11 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(ReTweetiePlugin, sharedInstance);
 
 + (void)load {
 	[ReTweetiePlugin sharedInstance];
-
+	
 	Method repostStatus = class_getInstanceMethod(NSClassFromString(@"TweetieStatusListViewController"), @selector(tweetieStatusCell:repostStatus:));
     const char *types = method_getTypeEncoding(repostStatus);
 	class_addMethod(NSClassFromString(@"TweetieStatusListViewController"), @selector(np_tweetieStatusCell:repostStatus:), (IMP)NPCellRepost, types);
 	[NSClassFromString(@"TweetieStatusListViewController") jr_swizzleMethod:@selector(tweetieStatusCell:repostStatus:) withMethod:@selector(np_tweetieStatusCell:repostStatus:) error:nil];
-	
 }
 
 - (id)init {
@@ -55,6 +44,33 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(ReTweetiePlugin, sharedInstance);
 		
 	}
 	return self;
+}
+
+- (void)retweetSheetDidEnd:(NSWindow *)sheet returnCode:(NSInteger)returnCode contextInfo:(NSDictionary *)contextInfo {
+	if (returnCode == NSAlertDefaultReturn) {
+		TweetieStatusTableViewCell *cell = [contextInfo objectForKey:@"cell"];
+		TwitterStatus *status = [contextInfo objectForKey:@"status"];
+		
+		if (cell == nil || status == nil) {
+			[contextInfo release];
+			return;
+		}
+		
+		NSArray *potentialSubviews = [[[cell window] contentView] subviews];
+		NSPredicate *predicate = [NSPredicate predicateWithFormat:@"class == %@", NSClassFromString(@"TweetieAccountTabsView")];
+		NSArray *result = [potentialSubviews filteredArrayUsingPredicate:predicate];
+		if ([result count] > 0) {
+			TweetieAccountTabsView *tabView = [result objectAtIndex:0];
+			TwitterAccount *account = [tabView selectedAccount];
+			if (account != nil) {
+				ABCallback *callback = [NSClassFromString(@"ABCallback") callbackWithTarget:[ReTweetiePlugin sharedInstance] selector:@selector(apiCallback:) info:nil];
+				TwitterAPI *api = [[NSClassFromString(@"TwitterAPI") alloc] initWithAccount:account callback:callback];
+				[api POST:[NSString stringWithFormat:@"statuses/retweet/%@.xml", status.statusID] body:nil callback:callback];
+				
+			}
+		}
+	}
+	[contextInfo release];
 }
 
 - (void)apiCallback:(id)info {
